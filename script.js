dbd8d68d80cf1081376ad44916916c5a2e5a88a3
Jen Evans <109816068+jenevans937@users.noreply.github.com>
2022-08-06 15:13:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+var click_count;
+
+
+click_count = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
+  event.target.innerText = click_count;
+
+});
